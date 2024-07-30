7b7be85def4e0abd7161ddca8c2b4ea36b1e9599
Cade Mitchell <cademitchell7@outlook.com>
2024-07-30 14:06:46 -0400

    menu selector function implemented.

--- Cauldron.py
+++ Cauldron.py
@@ -1,6 +1,6 @@
-
+from typing import Callable
+import tkinter as tk
 from tkinter import filedialog
-
 import json
 import os
 
@@ -9,27 +9,41 @@ def clear():
 
 def create_new_file():
     # Implement file creation logic here
+    root = tk.Tk()
     new_File_Path = filedialog.asksaveasfilename(defaultextension=".json", confirmoverwrite=True, initialfile="NewBrew")
     if new_File_Path:
         with open(new_File_Path, "w") as file:
             file.write("Test")
+    root.destroy()
             
 
 def open_file():
+    root = tk.Tk()
+    root.withdraw()
     file_path = filedialog.askopenfilename()
     if file_path:
         # Implement file opening logic here
         print("Opening file:", file_path)
+    root.destroy()
 
-def menu(options: list[tuple[str,function]]):
+def menu(options: list[tuple[str, Callable]]) -> Callable:
     clear()
     for key, value in enumerate(options):
-        print(f"({key}) - {value[0]}")
-    user = input("Option: ", end = "")
+        print(f"({key+1}) - {value[0]}")
+    user = input("Select: ")
+    
+    for key, value in enumerate(options):
+        if int(user)-1 == key:
+            return options[int(user)-1][1]
+    menu(options)
         
+
 def Main():
     clear()
+    test = menu([("Open File", open_file), ("Create New File", create_new_file)])
+    test()
     
+
     
 if __name__ == "__main__":
     Main()
