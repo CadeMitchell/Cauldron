8834acb54c87532bb8e8b4a6a50c287f63e8eb33
Cade Mitchell <cademitchell7@outlook.com>
2024-08-01 15:45:57 -0400

    Working on a main edit screen that will add option as they are added to your brew.

--- Cauldron.py
+++ Cauldron.py
@@ -7,9 +7,8 @@ def clear():
 
 class FileManagment:
     current_brew = None
-       
+    runnig = True
     def create_file():
-        # Implement file creation logic here
         FileManagment.current_brew = FileManagment.load_file(new_file=True)
         Navigation.new_meta_setup()
         FileManagment.save_file(FileManagment.current_brew)
@@ -52,6 +51,7 @@ class Navigation:
         clear()
         sources = FileManagment.current_brew["_meta"]["sources"][0]
         input("The setup process for the basic file data is now taking place. This will be documentation related to your homebrew.\nIf you make a mistake or change your mind you will be able to edit this later.\nPress ENTER to continue.")
+        clear()
         sources["json"] = input("Please enter a json identifier for your brew file. It must be completely unique to any other homebrew.\nThis is only used as an identifier and will not show up anywhere other than here. Just make it related and unique to the project.\n(Minimum of 6 characters, No Spaces, No Symbols, No Numbers)\nJSON: ")
         clear()
         sources["abbreviation"] = input("Please enter an abbreviation for your brew. (e.g. PHB for 'Player Hand Book')\nAbbreviation: ")
@@ -66,8 +66,36 @@ class Navigation:
         
         
 def Main():
-    initialize = Navigation.menu_generator([("Load Homebrew File", FileManagment.load_file), ("Create Homebrew File", FileManagment.create_file)])
-    initialize()
+    #initialize = Navigation.menu_generator([("Load Homebrew File", FileManagment.load_file), ("Create Homebrew File", FileManagment.create_file)])
+    #initialize()
+    brew_editor_lookup={
+        "_meta": Navigation.edit_meta,
+        "action": Navigation.edit_action,
+        "background": Navigation.edit_background,
+        "boon": Navigation.edit_boon,
+        "class": Navigation.edit_class,
+        "condition": Navigation.edit_condition,
+        "cult": Navigation.edit_cult,
+        "deity": Navigation.edit_deity,
+        "disease": Navigation.edit_diesease,
+        "feat": Navigation.edit_feat,
+        "hazard": Navigation.edit_hazard,
+        "item": Navigation.edit_item, #This will have some sub categories as their are several keys that fall into this category
+        "language": Navigation.edit_meta,
+        "object": Navigation.edit_meta,
+        "optionalfeature": Navigation.edit_optionalfeature, #This puts a dict under the _meta key
+        "race": Navigation.edit_race,
+        "reward": Navigation.edit_reward,
+        "subclass": Navigation.edit_subclass,
+        "table": Navigation.edit_table,
+        "varientrule": Navigation.edit_varientrule,
+        "vehicle": Navigation.edit_vehicle,
+    }
+    
+    while FileManagment.runnig:
+        brew_options = ["Edit "+str(item) for item in FileManagment.current_brew.keys()]
+        brew_options_editors = [brew_editor_lookup.get(item, None) for item in brew_options]
+        Navigation.menu_generator(zip(brew_options, brew_options_editors))
 
     
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Cauldron
+
+<p>This project is intended to make a tool that helps with making homebrew for use with 5eTools.<br>This is a project is its very early stage and is a WIP. This is my first personal project so don't roast my code too hard.</p>
+
+<p>The current goal is to get a cmd based program made that can encompass all of the possible Homebrew you can add to the site. After the program is fully functional, I want to clean up my code and add an interface.</p>
