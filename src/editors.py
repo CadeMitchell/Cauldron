0601c0954ab1fc81c57311750351d63c5f31af8c
Cade Mitchell <cademitchell7@outlook.com>
2024-09-06 16:44:51 -0400

    Made progress on the updated Meta editor

--- src/editors.py
+++ src/editors.py
@@ -3,7 +3,7 @@ This module if for housing the editor class and its subclasses for each editor.
 """
 from .utilities import menu_generator as mg
 from .utilities import clear as cl
-from typing import Callable
+from .file_managment import load_file as lf
 
 class base_editor:
     def __init__(self, brew: dict) -> None:
@@ -71,20 +71,54 @@ class meta_editor(base_editor):
     def __init__(self, brew: dict) -> None:
         super().__init__(brew)
         self.module = brew["_meta"]
-        self.primary_meta = self.module[0]
 
     def menu(self):
         while True:
-            choice = mg([("Exit Meta Editor", "Exit"), ("Edit a Meta Source", "Edit Meta"), ("Add a Meta Source", "Add Meta"), ("Delete a Meta Source", "Delete Meta")],
+            choice = mg([("Exit Meta Editor", "Exit"), ("Edit a Meta Source", "Edit Meta"), ("Add a Meta Source", "Add Source"), ("Delete a Meta Source", "Delete Source")],
                               """A meta is the part of the file that identifies your content. When adding things to your homebrew it will likley refrence things here
-                              You may have multiple metas per document (I don't recommend this), but for this program you will have a primary meta for file sorting purposes."""
-                              )
+                              You may have multiple metas sources per document (I don't recommend this), but for this program you will need atleast 1 source."""
+                        )
             
             if choice == "Exit":
                 break
             elif choice == "Edit Meta":
                 pass
-            elif choice == "Add Meta":
+            elif choice == "Add Source":
+                self._add_source()
+            elif choice == "Delete Source":
+                if self.module["sources"].len() > 1:
+                    self._delete_source()
+                else:
+                    input("You only have 1 source. Please add another before attempting to remove the final one.\n(Press ENTER to continue.)")
+            
+    def _add_source(self):
+        new_source = lf("Build_Templates\\_meta.json")["_meta"]["sources"][0]
+        user = input("Please enter the unique identifier you would like for your Homebrew file.\nThis must be 6 or more characters, no symbols, unique to all homebrews.\n(Leave INPUT blank to cancel)\nID: ")
+        if user:
+            new_source["json"] = user
+            self.module["sources"].append(new_source)
+            input(f"Source {user} added.\n(Press ENTER to continue)\n")
+        input("New Source cancelled.\n(Press ENTER to continue)\n")
+        
+    def _delete_source(self):
+        while True:
+            sources = [(source["json"], index) for index, source in enumerate(self.module["sources"])]
+            choice = mg([("Exit", "Exit")].extend(sources), "Select which source you would like to remove.")
+            if choice == "Exit":
+                input("Source delete cancelled.\n(Press ENTER to continue)")
+                break
+            else:
+                self.module["sources"].pop(choice)
+                input("Source deleted.\n(Press ENTER to continue)")
+            
+    def _choose_source_menu(self):
+        while True:
+            sources = [(source["json"], index) for index, source in enumerate(self.module["sources"])]
+            choice = mg([("Exit", "Exit")].extend(sources), "Select which source you would like to edit.")
+            if choice == "Exit":
+                break
+            else:
                 pass
-            elif choice == "Delete Meta":
-                pass
+            
+    def _edit_source_menu(self):
+        pass
